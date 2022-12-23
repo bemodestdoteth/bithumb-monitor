420fe00d75d615e67f6f14479c6b2fb4378ae4d7
bemodestdoteth <cody.shin@outlook.com>
2022-12-23 15:16:32 +0900

    change config

--- selenium_config.py
+++ selenium_config.py
@@ -6,7 +6,6 @@ def os_selection(user_agent):
     # Selenium on Linux
     if os.name == 'posix':
         # To bypass headless block
-        user_agent = 'Chrome/102.0.5005.61'
         chrome_options.page_load_strategy = 'eager'
         chrome_options.add_argument('--headless')
         chrome_options.add_argument('--disable-gpu')
@@ -28,5 +27,9 @@ def os_selection(user_agent):
         Preserve user cookies
         t.ly/uDkv
         '''
-    driver = webdriver.Chrome(options=chrome_options, executable_path=os.path.join(os.path.dirname(__file__), 'chromedriver'))
+    driver = webdriver.Chrome(options=chrome_options)
     return driver
+# Change to a synchronous activity
+def update_chromedriver():
+    if os.name == 'posix': # Only on linux
+        os.system('version=$(curl -s "https://chromedriver.storage.googleapis.com/LATEST_RELEASE") & wget -qP /tmp/ "https://chromedriver.storage.googleapis.com/${version}/chromedriver_linux64.zip" & sudo unzip -o /tmp/chromedriver_linux64.zip -d ~/works/python-bithumb-monitor')
