6da33694d748cbde313547575d6bf3e6bd6a8a84
bemodestdoteth <cody.shin@outlook.com>
2023-01-01 03:41:12 +0900

    change status file

--- status.py
+++ status.py
@@ -4,6 +4,7 @@ from fp.fp import FreeProxy
 # FreeProxy for preventing IP ban
 from pybithumb import Bithumb
 from dotenv import load_dotenv
+from db import get_all_coins
 from config import print_n_log
 from db import get_working_proxy
 
@@ -54,22 +55,32 @@ def maedo(coin):
     # Get Balance and Selling Amount
     # (총 잔고, 거래중인 암호화폐 수량, 보유 원화, 주문에 사용된 원화)
     balance = bithumb.get_balance(coin)
-    amount = balance[0]
-
-    # Sell Coin
-    if amount > 0:
-        # Wait for other bot and person buying up coins
-        time.sleep(10)
-        result = bithumb.sell_market_order(coin, amount, 'KRW')
-        print_n_log(result)
+    if balance["status"]:
+        print_n_log("Couldn't connect to Bithumb API. Skip Selling...")        
     else:
-        print_n_log('You don\'t hane {} in your balances. Skip selling...'.format(coin))
+        amount = balance[0]
+        # Sell Coin
+        if amount > 0:
+            # Wait for other bot and person buying up coins
+            time.sleep(10)
+            result = bithumb.sell_market_order(coin, amount, 'KRW')
+            print_n_log(result)
+        else:
+            print_n_log('You don\'t hane {} in your balances. Skip selling...'.format(coin))
 def get_ticker():
     tickers = {}
     omits = ['BTC', 'ETH', 'XRP', 'BCH', 'EOS', 'TRX']
     tickers["KRW"] = Bithumb.get_tickers('KRW')
     tickers["BTC"] = list((btc_ticker for btc_ticker in Bithumb.get_tickers('btc') if btc_ticker not in tickers['KRW']))
     return tickers
+def get_buy_sell_coins():
+    coin_list = []
+    for coin in get_all_coins():
+        if coin["groups"] != "":
+            (coin_list.append(*group) for group in coin["groups"].split(","))
+        else:
+            coin_list.append(coin["name"])
+    return coin_list
 def get_status():
     url = "https://api.bithumb.com/public/assetsstatus/ALL"
     headers = {"accept": "application/json"}
@@ -77,11 +88,12 @@ def get_status():
     proxy_timer = 0
     ticker_timer = 0
     proxy = get_working_proxy()
-    tickers = get_ticker()
+    #tickers = get_ticker()
+    coin_list = get_buy_sell_coins()
 
     while True:
         try:
-            response = requests.get(url,proxies=proxy, headers=headers)
+            response = requests.get(url,proxies={"http": proxy}, headers=headers)
             api_n = json.loads(response.text)['data']
 
             # If there's no status.json, skip the whole process and save the first result as status.json
@@ -96,11 +108,11 @@ def get_status():
                     elif api_n[coin] != api_o[coin]:
                         if api_n[coin]['withdrawal_status'] == 0:
                         #if api_n[coin]['withdrawal_status'] == 1:
-                            if coin in tickers:
+                            if coin in coin_list:
                                 print_n_log('{} withdrawal closed.'.format(coin, coin))
                                 maedo(coin)
                         elif api_n[coin]['withdrawal_status'] == 0:
-                            if coin in tickers:
+                            if coin in coin_list:
                                 print_n_log('{} withdrawal opened'.format(coin))
                                 #print_n_log('Selling {}...'.format(coin))
                                 #maesoo(coin)
@@ -121,11 +133,13 @@ def get_status():
             if proxy_timer >= 300:
                 print_n_log('Proxy change timer reached. Changing proxy...')
                 proxy = get_working_proxy()
-                print_n_log('Proxy changed. A new proxy is: {}'.format(proxy['http']))
+                print_n_log('Proxy changed. A new proxy is: {}'.format(proxy))
                 proxy_timer = 0
 
             time.sleep(2)
-
         except Exception as e:
             logging.info(e)
+            raise Exception(e)
             break
+
+#get_status()
