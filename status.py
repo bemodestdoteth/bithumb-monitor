f7e3f6720fd4c788ac67a5fb42042de42cebe837
bemodestdoteth <cody.shin@outlook.com>
2023-01-06 09:08:05 +0900

    modify while try

--- status.py
+++ status.py
@@ -67,68 +67,69 @@ def maedo(coin):
         else:
             print_n_log('You don\'t hane {} in your balances. Skip selling...'.format(coin))
 def get_status():
-    try:
-        url = "https://api.bithumb.com/public/assetsstatus/ALL"
-        headers = {"accept": "application/json"}
+    url = "https://api.bithumb.com/public/assetsstatus/ALL"
+    headers = {"accept": "application/json"}
 
-        # Initialize variables
-        file_changed = False
-        proxy_timer = 0
-        proxy = get_working_proxy()
+    # Initialize variables
+    file_changed = False
+    proxy_timer = 0
+    proxy = get_working_proxy()
 
-        # Get old api result before moving onto the loop for efficiency
-        with open('./status.json','r') as f:
-            api_o = json.loads(f.readline())
+    # Get old api result before moving onto the loop for efficiency
+    with open('./status.json','r') as f:
+        api_o = json.loads(f.readline())
 
-        while True:
-                # If there's no status.json, skip the whole process and save the first result as status.json
-                if file_changed and os.path.isfile('./status.json'):
-                    with open('./status.json','r') as f:
-                        api_o = json.loads(f.readline())
+    while True:
+        try:
+            # If there's no status.json, skip the whole process and save the first result as status.json
+            if file_changed and os.path.isfile('./status.json'):
+                with open('./status.json','r') as f:
+                    api_o = json.loads(f.readline())
 
-                # Get new data from Bithumb API
-                api_n = json.loads(requests.get(url ,proxies={"http": proxy}, headers=headers).text)['data']
+            # Get new data from Bithumb API
+            api_n = json.loads(requests.get(url ,proxies={"http": proxy}, headers=headers).text)['data']
 
-                if api_n != api_o:
-                    for coin in api_n.keys():
-                        if coin not in tuple(api_o.keys()):
-                            print_n_log('New coin: {}'.format(coin))
-                        elif api_n[coin] != api_o[coin]:
-                            if api_n[coin]['withdrawal_status'] == 0:
-                                if coin in get_buy_sell_coins():
-                                    print_n_log('{} withdrawal closed.'.format(coin, coin))
-                                    maedo(coin)
-                            elif api_n[coin]['withdrawal_status'] == 0:
-                                if coin in get_buy_sell_coins():
-                                    print_n_log('{} withdrawal opened'.format(coin))
-                                    #print_n_log('Selling {}...'.format(coin))
-                                    #maesoo(coin)
-                    with open('./status.json','w') as f:
-                        f.write(json.dumps(api_n))
-                        file_changed = True
-                        print_n_log(msg='Suucessfully renewed deposit and withdraw status.')
-                else:
-                        print_n_log(msg='Keep watching deposit and withdraw status...')
+            if api_n != api_o:
+                for coin in api_n.keys():
+                    if coin not in tuple(api_o.keys()):
+                        print_n_log('New coin: {}'.format(coin))
+                    elif api_n[coin] != api_o[coin]:
+                        if api_n[coin]['withdrawal_status'] == 0:
+                            if coin in get_buy_sell_coins():
+                                print_n_log('{} withdrawal closed.'.format(coin, coin))
+                                maedo(coin)
+                        elif api_n[coin]['withdrawal_status'] == 0:
+                            if coin in get_buy_sell_coins():
+                                print_n_log('{} withdrawal opened'.format(coin))
+                                #print_n_log('Selling {}...'.format(coin))
+                                #maesoo(coin)
+                with open('./status.json','w') as f:
+                    f.write(json.dumps(api_n))
+                    file_changed = True
+                    print_n_log(msg='Suucessfully renewed deposit and withdraw status.')
+            else:
+                    print_n_log(msg='Keep watching deposit and withdraw status...')
 
-                gc.collect()
-                proxy_timer = proxy_timer + 1
+            gc.collect()
+            proxy_timer = proxy_timer + 1
 
-                # Change proxy every 10 minutes
-                if proxy_timer >= 300:
-                    print_n_log('Proxy change timer reached. Changing proxy...')
-                    proxy = get_working_proxy()
-                    print_n_log('Proxy changed. A new proxy is: {}'.format(proxy))
-                    proxy_timer = 0
+            # Change proxy every 10 minutes
+            if proxy_timer >= 300:
+                print_n_log('Proxy change timer reached. Changing proxy...')
+                proxy = get_working_proxy()
+                print_n_log('Proxy changed. A new proxy is: {}'.format(proxy))
+                proxy_timer = 0
 
-                time.sleep(2)
-    except ConnectionError:
-        print_n_log("Failed to fetch api. Changing proxy and try again...")
-        proxy = FreeProxy(rand=True).get().replace("http://", "")
-        print_n_log("Now Connected to: {}".format(proxy))
-    except Exception as e:
-        asyncio.run(send_error_message("Bithumb Deposit and Withdraw Status", e))
-        print_n_log(e)
-        raise Exception(e)
+            time.sleep(2)
+        except ConnectionError:
+            asyncio.run(send_error_message("Bithumb Deposit and Withdraw Status", "Failed to fetch api. Changing proxy and try again..."))
+            print_n_log("Failed to fetch api. Changing proxy and try again...")
+            proxy = FreeProxy(rand=True).get().replace("http://", "")
+            print_n_log("Now Connected to: {}".format(proxy))
+        except Exception as e:
+            asyncio.run(send_error_message("Bithumb Deposit and Withdraw Status", e))
+            print_n_log(e)
+            raise Exception(e)
 
 if __name__ == "__main__":
     get_status()
